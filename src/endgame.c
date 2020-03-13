5a8db0443600be3fdbc7cac26cf71f1f49ddfb12
Alexis THOMAS <alexis.thomas@epitech.eu>
2020-03-13 11:32:56 +0100

    [fix] life

--- src/endgame.c
+++ src/endgame.c
@@ -23,7 +23,7 @@ all_t endgame_part(all_t all)
         all.cn.reset_map_v = 0;
         all.cn.max = 1;
         all.cn.number_waves = 1;
-        all.str.line = split_line(read_fonction("src/create_waves.txt"), 0, all);
+        all.str.line = split_line(read_fonction("src/create_waves.txt"), 1, all);
     }
     if (check_click(all.tex.restart, 138, 125, all) == 1 && all.cn.life == 0) {
         all.cn.life = 150;
@@ -31,7 +31,7 @@ all_t endgame_part(all_t all)
         all.cn.reset_map = 0;
         all.cn.reset_map_v = 0;
         all.cn.number_waves = 1;
-        all.str.line = split_line(read_fonction("src/create_waves.txt"), 0, all);
+        all.str.line = split_line(read_fonction("src/create_waves.txt"), 1, all);
         all.cn.max = 1;
     }
     return (all);
--- src/life.c
+++ src/life.c
@@ -40,23 +40,23 @@ all_t gest_life2(all_t all, sfSprite *sprite, int i)
 {
     char *life = malloc(sizeof(char) * 4);
 
-    if (all.str.line[i] == '1' && all.pos.ballon.y <= 36) {
+    if (all.str.line[i] == '1' && all.pos.ballon.y == 36) {
         all.cn.life = all.cn.life - 1;
         set_pos(sprite, 20, 40);
     }
-    if (all.str.line[i] == '2' && all.pos.ballon.y <= 36) {
+    if (all.str.line[i] == '2' && all.pos.ballon.y == 36) {
         all.cn.life = all.cn.life - 2;
         set_pos(sprite, 20, 40);
     }
-    if (all.str.line[i] == '3' && all.pos.ballon.y <= 36) {
+    if (all.str.line[i] == '3' && all.pos.ballon.y == 36) {
         all.cn.life = all.cn.life - 3;
         set_pos(sprite, 20, 40);
     }
-    if (all.str.line[i] == '4' && all.pos.ballon.y <= 36) {
+    if (all.str.line[i] == '4' && all.pos.ballon.y == 36) {
         all.cn.life = all.cn.life - 4;
         set_pos(sprite, 20, 40);
     }
-    if (all.str.line[i] == '5' && all.pos.ballon.y <= 36) {
+    if (all.str.line[i] == '5' && all.pos.ballon.y == 35) {
         all.cn.life = all.cn.life - 5;
         set_pos(sprite, 20, 40);
     }
